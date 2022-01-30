e56a496a0b9aa8488c7a8477c6f21e8791c538a5
evshaddock <evshaddock@gmail.com>
2022-01-29 18:17:05 -0600

    what strange and exciting ways will this break everything?

---
+++ target/classes/example_plugin.lua
@@ -0,0 +1,9 @@
+-- Example plugin for Stitch plugin system
+function OnServerInit()
+    -- Call something from the main server class
+    minecraft_server:log("This is an example plugin hooking into the minecraft server class")
+    return "HOOK_COMPLETE" -- Hooks will eventually have return states. For now this is a placeholder.
+end
+
+function OnBlockPlace()
+end
